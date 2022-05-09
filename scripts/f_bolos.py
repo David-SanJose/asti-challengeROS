f068f633c42ee51662bfe51a397eebf634126419
nerfshacopls <dsjgdavid@gmail.com>
2022-05-09 08:38:45 -0700

    commit final

--- scripts/f_bolos.py
+++ scripts/f_bolos.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 import rospy
-from scripts.f_barredoraTeo import ESTADO_CORREGIR_IZQ, ESTADO_INDETERMINADO, ESTADO_RECONOCIMIENTO
-from scripts.f_laberinto import ESTADO_CORREGIR_DER
 from std_msgs.msg import String
 from std_msgs.msg import Int16MultiArray
 from asti_prueba.msg import Ldrs
 
+from std_msgs.msg import Int16
+
 import time
 
 pub = rospy.Publisher('orden_mov', String, queue_size=10)
+pub_servo = rospy.Publisher('orden_grados_Servo', Int16, queue_size=10)
 rospy.init_node('bolos_node', anonymous=False)
 rate = rospy.Rate(10) # 10hz
 
@@ -16,25 +17,27 @@ ldrs = Ldrs()
 UltrSoni2 = Int16MultiArray()
 data = []
 
-# Otras variables
-estadoAnterior = ESTADO_INDETERMINADO
+
 
 #Limites de distanciacon las paredes
-limites_inf = [8,20, 15]
-limites_sup = [60,99999, 25]
+limites_inf = [5,20, 5]
+limites_sup = [60,99999, 20]
 MAX_LIMITE_EXTREMO = [60,999999,40]
 # Convenciones para CONSTANTES
 IZQ, CEN, DER = [0,1,2]
 INDETERMINADO, INFERIOR_AL_LIMITE, CENTRADO_ENTRE_LIMITES, SUPERIOR_AL_LIMITE, EXTREMO_LEJANO_AL_LIMITE = [-99,-1,0,1, 99]
 #Estado
 ESTADO_INDETERMINADO, ESTADO_AVANZAR_1, ESTADO_CORREGIR_IZQ, ESTADO_CORREGIR_DER, ESTADO_GIRO_IZQ = ["I", "A1", "CI", "CD", "GI"]
-ESTADO_AVANZAR_2, ESTADO_MOV_IZQ, ESTADO_DISPARAR = ["A2", "MI", "SHOOT"]
-ESTADO_RECONOCIMIENTO = ["REC"]
+ESTADO_AVANZAR_2, ESTADO_DET_LDR_I, ESTADO_DET_LDR_D, ESTADO_DISPARAR = ["A2", "LDR-I","LDR-D", "SHOOT"]
+ESTADO_DETEC_PAREDES,  ESTADO_MOV_IZQ,  ESTADO_MOV_DER = ["DET_PAREDES", "MI", "MD"]
 
 #ACCIONES
 A_AVANZAR, A_GIRO_IZQ, A_GIRO_DER, A_ATRAS, A_STOP, A_MOV_IZQ, A_MOV_DER = ["avanza", "gizq", "gder", "atras", "stop", "mizq", "mder"]
+A_VEL_MEDIO, A_VEL_RAPIDO = ["VEL medio", "VEL rapido"]
+
 # Lista de acciones segun el ciclo de reconocimiento
 
+# Otras variables
 
 
 
@@ -44,7 +47,7 @@ def ordenDeMoviviento(movimiento_str):
 def subsLDRs_data(data):
     global ldrs
     ldrs = data
-    rospy.loginfo(rospy.get_caller_id()+" LDR He recibido datos" + str(data.ldr1) + str(ldrs.ldr1))
+    #rospy.loginfo(rospy.get_caller_id()+" LDR He recibido datos" + str(data.ldr1) + str(ldrs.ldr2))
 
 def subsUltrasonidos(array_data):
     global UltrSoni2
@@ -69,7 +72,10 @@ def getEstadoDePosicionConPared(data_fija, PARED):
 
 def accionSegunEstado(estado):
     if estado == ESTADO_INDETERMINADO:
+        ordenDeMoviviento(A_VEL_RAPIDO)
+        time.sleep(0.3)
         ordenDeMoviviento(A_STOP)
+        time.sleep(0.3)
     
     elif estado == ESTADO_AVANZAR_1:
         ordenDeMoviviento(A_AVANZAR)
@@ -82,63 +88,166 @@ def accionSegunEstado(estado):
 
     elif estado == ESTADO_GIRO_IZQ:
         print("> GIRANDO A LA IZQUIERDA")
+        
+        ordenDeMoviviento(A_AVANZAR)
+        time.sleep(0.8)
         ordenDeMoviviento(A_STOP)
         time.sleep(1)
         ordenDeMoviviento(A_GIRO_IZQ)
-        time.sleep(1.7)
+        time.sleep(1.2)
+        ordenDeMoviviento(A_AVANZAR)
+        time.sleep(1)
+        ordenDeMoviviento(A_VEL_MEDIO)
 
     elif estado == ESTADO_AVANZAR_2:
         ordenDeMoviviento(A_AVANZAR)
     
+
+    elif estado == ESTADO_DET_LDR_I:
+        ordenDeMoviviento(A_STOP)
+        time.sleep(0.5)
+        ordenDeMoviviento(A_ATRAS)
+        time.sleep(0.8)
+        ordenDeMoviviento(A_GIRO_IZQ)
+        time.sleep(0.3)
+
+    elif estado == ESTADO_DET_LDR_D:
+        ordenDeMoviviento(A_STOP)
+        time.sleep(0.5)
+        ordenDeMoviviento(A_ATRAS)
+        time.sleep(0.8)
+        ordenDeMoviviento(A_GIRO_DER)
+        time.sleep(0.3)
+    
+    elif estado == ESTADO_DETEC_PAREDES:
+        ordenDeMoviviento(A_STOP)
+        time.sleep(0.8)
+
+    elif estado == ESTADO_MOV_DER:
+        print("MOVIMIENTO IZQUIERDA")
+        ordenDeMoviviento(A_VEL_RAPIDO)
+        time.sleep(0.3)
+        ordenDeMoviviento(A_ATRAS)
+        time.sleep(0.8)
+        ordenDeMoviviento(A_STOP)
+        time.sleep(0.4)
+        ordenDeMoviviento(A_MOV_DER)
+        time.sleep(0.6)
+        ordenDeMoviviento(A_STOP)
+        time.sleep(0.2)
+        ordenDeMoviviento(A_VEL_MEDIO)
+        time.sleep(0.1)
+        print("FIN MOVIMIENTO IZQUIERDA")
+    
     elif estado == ESTADO_MOV_IZQ:
+        print("MOVIMIENTO IZQUIERDA")
+        ordenDeMoviviento(A_VEL_RAPIDO)
+        time.sleep(0.3)
+        ordenDeMoviviento(A_ATRAS)
+        time.sleep(0.8)
+        ordenDeMoviviento(A_STOP)
+        time.sleep(0.4)
         ordenDeMoviviento(A_MOV_IZQ)
+        time.sleep(0.6)
+        ordenDeMoviviento(A_STOP)
+        time.sleep(0.2)
+        ordenDeMoviviento(A_VEL_MEDIO)
+        time.sleep(0.1)
+        print("FIN MOVIMIENTO IZQUIERDA")
+
+    
+    elif estado == ESTADO_DISPARAR:
+        ordenDeMoviviento(A_STOP)
+        time.sleep(1)
+        ordenDeMoviviento(A_ATRAS)
+        time.sleep(1)
+        pub_servo.publish(180)
 
 
 
 def cambiarDeEstado(estado, lista_estados_pared):
     global estadoAnterior
+    global ldrs
+    global data
 
     estado_pared_izq = lista_estados_pared[0]
     estado_pared_cen = lista_estados_pared[1]
     estado_pared_der = lista_estados_pared[2]
 
     if estado == ESTADO_INDETERMINADO:
-        if estado_pared_der == INFERIOR_AL_LIMITE:
-            return ESTADO_CORREGIR_IZQ
-        elif estado_pared_der == SUPERIOR_AL_LIMITE:
-            return ESTADO_CORREGIR_DER
-        elif estado_pared_der == CENTRADO_ENTRE_LIMITES:
+        if estado_pared_der == CENTRADO_ENTRE_LIMITES:
             return ESTADO_AVANZAR_1
     
-    if estado == ESTADO_AVANZAR_1:
+    elif estado == ESTADO_AVANZAR_1:
         if estado_pared_izq == EXTREMO_LEJANO_AL_LIMITE:
             return ESTADO_GIRO_IZQ
         elif estado_pared_der == INFERIOR_AL_LIMITE:
             return ESTADO_CORREGIR_IZQ
-        elif estado_pared_der == SUPERIOR_AL_LIMITE:
+        elif estado_pared_izq == INFERIOR_AL_LIMITE:
             return ESTADO_CORREGIR_DER
 
-    if estado == ESTADO_CORREGIR_IZQ:
+    elif estado == ESTADO_CORREGIR_IZQ:
         if estado_pared_der != INFERIOR_AL_LIMITE:
-            return estadoAnterior
+            return ESTADO_AVANZAR_1
     
-    if estado == ESTADO_CORREGIR_DER:
-        #posible problema con extremo ?
-        if estado_pared_der != SUPERIOR_AL_LIMITE:
-            return estadoAnterior
+    elif estado == ESTADO_CORREGIR_DER:
+        if estado_pared_izq != INFERIOR_AL_LIMITE:
+            return ESTADO_AVANZAR_1
 
-    if estado == ESTADO_GIRO_IZQ:
+    elif estado == ESTADO_GIRO_IZQ:
         return ESTADO_AVANZAR_2
 
-    if estado == ESTADO_AVANZAR_2:
+    elif estado == ESTADO_AVANZAR_2:
         # TODO AQUI VA condicion de LDRS
-        if estado_pared_der == INFERIOR_AL_LIMITE:
+        
+
+        if ldrs.ldr1 and ldrs.ldr2:
+            return ESTADO_DETEC_PAREDES
+        elif ldrs.ldr1:
+            return ESTADO_DET_LDR_I
+        elif ldrs.ldr2:
+            return ESTADO_DET_LDR_D
+        elif estado_pared_der == INFERIOR_AL_LIMITE:
             return ESTADO_CORREGIR_IZQ
-        elif estado_pared_der == SUPERIOR_AL_LIMITE:
+        elif estado_pared_izq == INFERIOR_AL_LIMITE:
             return ESTADO_CORREGIR_DER
 
-    
+    elif estado == ESTADO_DET_LDR_I:
+        ldrs = ldrs
+        if ldrs.ldr1 and ldrs.ldr2:
+            return ESTADO_DETEC_PAREDES
+        elif not ldrs.ldr1 and not ldrs.ldr2:
+            return ESTADO_AVANZAR_2
+        elif ldrs.ldr1:
+            return ESTADO_DET_LDR_I
+        elif ldrs.ldr2:
+            return ESTADO_DET_LDR_D
 
+    elif estado == ESTADO_DET_LDR_D:
+        ldrs = ldrs
+        if ldrs.ldr1 and ldrs.ldr2:
+            return ESTADO_DETEC_PAREDES
+        elif not ldrs.ldr1 and not ldrs.ldr2:
+            return ESTADO_AVANZAR_2
+        elif ldrs.ldr1:
+            return ESTADO_DET_LDR_I
+        elif ldrs.ldr2:
+            return ESTADO_DET_LDR_D
+
+    elif estado == ESTADO_DETEC_PAREDES:
+        dist_izq = data[IZQ]
+        dist_der = data[DER]
+
+        res = dist_izq - dist_der
+        if res < -5 :
+            return ESTADO_MOV_DER
+        elif res > 5:
+            return ESTADO_MOV_IZQ
+        else:
+            return ESTADO_DISPARAR
+
+    elif estado == ESTADO_MOV_IZQ or estado == ESTADO_MOV_DER:
+        return ESTADO_AVANZAR_2
 
 
 
@@ -148,12 +257,13 @@ def cambiarDeEstado(estado, lista_estados_pared):
 def main():
     #Se declara el subscriber del Ultrasonido
     rospy.Subscriber('Ultrasonidos_data', Int16MultiArray, subsUltrasonidos)
+    rospy.Subscriber('LDRs_data', Ldrs, subsLDRs_data)
 
     # Estado en el que se encuentra el robot
-    global estadoAnterior
+
     estado = ESTADO_INDETERMINADO
 
-
+    
     #Entra al bucle de ROS
     while not rospy.is_shutdown():
         global data
@@ -170,11 +280,11 @@ def main():
         #Accion segun estado
         accionSegunEstado(estado)
         # cambio de estado
-        estadoAnterior = estado
+ 
         estado = cambiarDeEstado(estado, [dist_estado_izq, dist_estado_cen, dist_estado_der])
         
         try:
-            print("ESTADO ACTUAL: ", estado, "   --- DIST: ", data_fija[IZQ])
+            print("ESTADO ACTUAL: ", estado, "   --- DIST: ", data_fija)
         except:
             print("ESTADO ACTUAL: ", estado)
         ### FIN DEL CODIGO ###
@@ -201,6 +311,6 @@ def mainMockup():
 
 if __name__ == '__main__':
     try:
-        mainMockup()
+        main()
     except rospy.ROSInterruptException:
         pass
--- scripts/f_servoPales.py
+++ scripts/f_servoPales.py
@@ -2,17 +2,36 @@
 import rospy
 from std_msgs.msg import Int16
 
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + 'I heard %d', data.data)
+try:
+    import RPi.GPIO as GPIO
+except:
+    import Mock.GPIO as GPIO
 
-def motores():
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(24, GPIO.OUT)
+
+pwm = GPIO.PWM(24, 100)
+
+def AngleToDuty(angulo):
+    return float(angulo)/10. + 5.
+
+def callback(data):
+    posicion = data.data
+    pwm.ChangeDutyCycle(AngleToDuty(posicion))
+    print("Cambiando angulo a:", posicion)
+
+    
+
+def motorSERVO():
     # Solo deberia haber un nodo de motores running
     rospy.init_node('servopales_nodo', anonymous=False)
 
     rospy.Subscriber('orden_grados_Servo', Int16, callback)
 
+    pwm.start(AngleToDuty(0))
+
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
 if __name__ == '__main__':
-    motores()
+    motorSERVO()
